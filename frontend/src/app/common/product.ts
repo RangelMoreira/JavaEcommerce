5b4e8d4443399c8522c3741d1de4f4e4fa512434
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2022-05-01 19:28:06 -0300

    first commit

---
+++ frontend/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+  sku!: string;
+  name!: string;
+  description!: string;
+  unitPrice!: number;
+  imageUrl!: string;
+  active!: boolean;
+  unitsInStock!: number;
+  dateCrated!: Date;
+  lastUpdated!: Date;
+
+}
