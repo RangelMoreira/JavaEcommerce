eec07455dada81621b28c0c36516f389c8adff44
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2022-06-09 14:59:00 -0300

    Searching products by category

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -13,17 +13,18 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]>{
+  getProductList(theCategoryId: number): Observable<Product[]>{
 
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    // need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
 
       map(response => response._embedded.products)
 
     );
 
-
   }
-
 
 }
 
